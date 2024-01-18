62269dae7450df6f54dd547a63292dfee4da8a3b
Akbar-07 <116638921+Akbar-07@users.noreply.github.com>
2024-01-18 15:16:34 +0500

    Ishlaydi

---
+++ src/components/ProductCompareList/ProductCompareList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useComparisonsStore } from '@/app/stores/modules/comparison';
+import { defineAsyncComponent } from 'vue';
+import { storeToRefs } from 'pinia';
+const ProductCompare = defineAsyncComponent(() => import('@/components/ProductCompare/ProductCompare.vue'));
+const comparisonsStore = useComparisonsStore();
+const { comparisons, comparisonCount } = storeToRefs(comparisonsStore);
+const { comparisonRemove } = useComparisonsStore();
+</script>
+
+<template>
+    <div v-if="comparisonCount > 0" style="gap: 1.4vw; justify-content: center;flex-direction: row;" class="compare_list row">
+        <div class="for_comparisonCount" style="position: relative;" v-for="comparison in comparisons" @key="comparison?.data?.id">
+            <div @click="comparisonRemove(comparison?.data?.id)" class="compare_close">
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24"
+                    style="fill: white;transform: ;msFilter:;">
+                    <path
+                        d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z">
+                    </path>
+                </svg>
+            </div>
+        <ProductCompare :product="comparison"></ProductCompare>
+    </div>
+</div></template>
---
+++ src/components/ProductsViewed/ProductsViewed.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { defineAsyncComponent } from 'vue';
+import { useViewedsStore } from '@/app/stores/modules/viewed';
+import { storeToRefs } from 'pinia';
+import ProductButtonsDefault from '@/components/ProductButtonsDefault/ProductButtonsDefault.vue';
+const ProductCart = defineAsyncComponent(() => import('@/components/ProductCart/ProductCart.vue'));
+
+const viewedsStore = useViewedsStore();
+const { vieweds } = storeToRefs(viewedsStore);
+</script>
+
+<template>
+    <div class="last_catalog row">
+        <h2>ВЫ <b>СМОТРЕЛИ</b></h2>
+        <div class="last_catalog_list row">
+            <ProductCart v-for="product in vieweds" @key="product.id" :product="product">
+                <template v-slot:button-icons>
+                    <ProductButtonsDefault :product="product" />
+                </template>
+            </ProductCart>
+        </div>
+    </div>
+    <div class="pagination_div">
+        <div class="pagination_div_orange">
+            <h3>ПОКАЗАТЬ ЕЩЕ</h3>
+        </div>
+        <div class="pagination_div_number">
+            <h4 style="color: #FF781F">1</h4>
+            <h4>2</h4>
+            <h4>3</h4>
+            <h4>4</h4>
+            <h4>5</h4>
+            <h4>...</h4>
+            <h4>15</h4>
+        </div>
+        <div class="pagination_div_paginations">
+            <div class="pagination_pagination"></div>
+            <div id="for_gray" class="pagination_pagination"></div>
+        </div>
+    </div>
+</template>
